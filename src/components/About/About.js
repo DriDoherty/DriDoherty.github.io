3652f8fb653042fac57aafcad65c605c9a99b678
Adriana Doherty <dridoherty@gmail.com>
2019-02-07 17:00:55 -0500

    New React Portfolio

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+const About = () => (
+    <div className="aboutMe">
+        <h1 id="about_greeting">Hi, I'm Adriana.</h1>
+        <h2 id="about_whoIam">a software developer.</h2>
+        <h5>
+            I'm a full stack web developer with 10+ years of demonstrated experience in software development. I'm curious about technology and enjoy learning new languages, tools and environments.
+        </h5>
+        <h5>
+            Recently I've been building my skills in Javascript and the MERN stack through the Rutgers Coding Bootcamp, a Full Stack web development certification program. You can explore some of my recent projects on this site.
+        </h5>
+        <br />
+    </div>
+);
+
+export default About;
