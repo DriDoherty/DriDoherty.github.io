83b3bd70359dda32201361a8cea60d8e012f2291
Adriana Doherty <dridoherty@gmail.com>
2019-02-07 20:55:29 -0500

    Updates

---
+++ precache-manifest.f0bd4ed875f3af22cfa8ac6ae8854f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "759200a854ea5c947ab3",
+    "url": "/static/js/main.759200a8.chunk.js"
+  },
+  {
+    "revision": "42da34887e7d6ba85098",
+    "url": "/static/js/1.42da3488.chunk.js"
+  },
+  {
+    "revision": "759200a854ea5c947ab3",
+    "url": "/static/css/main.308fe9d8.chunk.css"
+  },
+  {
+    "revision": "ccaa5178df9f5b4e255cdab92877ec65",
+    "url": "/index.html"
+  }
+];
